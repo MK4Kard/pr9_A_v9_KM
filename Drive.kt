86988fae1263a442eb1bc478fe5e467a7f21ae69
MK4Kard <misha212032@gmail.com>
2025-03-04 20:43:35 +0500

    Update Drive.kt

--- Drive.kt
+++ Drive.kt
@@ -1,13 +1,27 @@
 package Train
 open class Drive (var number: String, var point: String, var days: Int, var timeArr: Int, var timePark: Int){
 
+    open fun Speed(speed: Double) {
+        if (speed < 45 || speed > 65){
+            println("Скорость не входит в диапазон")
+        }
+        else {
+            println("Скорость поезда: ${speed}")
+        }
+    }
+
     open fun path() {
         println("Поезд с номером $number\n" +
                 "Пункт назначения: $point")
     }
 
     open fun Info() {
-        println("Дней в пути: $days\n" +
-                "Время прибытия - $timeArr; Время стоянки - $timePark")
+        if (days != 0){
+            println("Дней в пути: $days\n" +
+                    "Время прибытия - $timeArr; Время стоянки - $timePark")
+        }
+        else {
+            println("Время прибытия - $timeArr; Время стоянки - $timePark")
+        }
     }
 }

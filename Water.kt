7d17115f390b950d86326cce71a437dceed59236
MK4Kard <misha212032@gmail.com>
2025-03-09 16:26:44 +0500

    Update Water.kt

--- Water.kt
+++ Water.kt
@@ -1,7 +1,6 @@
-import Product.*
 class Water(var litr: Double, var bubble: String, empName: String, empPrice: Double, empEd: String, empType: String, empCount: Int) : Products(empName, empPrice, empEd, empType, empCount) {
     private var calorie: Int = 0
-    public fun SetLitr(litr: Double){
+    fun SetLitr(litr: Double){
         if (litr <= 0){
             println("Литр не может быть меньше или равен 0")
         }
@@ -9,8 +8,7 @@ class Water(var litr: Double, var bubble: String, empName: String, empPrice: Dou
             this.litr = litr
         }
     }
-
-    public fun SetBubb(bubble: String){
+    fun SetBubb(bubble: String){
         if (bubble == "Да"){
             this.bubble = "Газированная"
         }
@@ -21,14 +19,27 @@ class Water(var litr: Double, var bubble: String, empName: String, empPrice: Dou
             this.bubble = "Не выбрано"
         }
     }
-
     override fun Info() {
-        println("Вода - ${name}, ${litr} л")
-        println("${bubble}, количество калорий: ${calorie}")
-        println("вид - ${type}, количество: ${count}")
-    }
+        if (count > 0){
+            println("Вода - ${name}, ${litr} л")
+            println("${bubble}, количество калорий: ${calorie}")
+            println("вид - ${type}, количество: ${count}")
+        }
+        else {
+            println("Количество не может быть меньше 0")
+        }
 
+    }
     override fun Weight() {
-        println("${price} за 1 ${ed}")
+        if (price > 1) {
+            println("${price} за 1 ${ed}")
+        } else {
+            println("Цена должна быть больше 1")
+        }
+    }
+    override fun Sum(){
+        if (count > 0 && price > 1){
+            println("Итого за ${count} ед надо заплатить ${String.format("%.2f",price*litr*count)} руб")
+        }
     }
 }

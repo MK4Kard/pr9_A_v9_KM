4ab4901b0fe4f8e3b63e505745ae4f14bf7209c6
MK4Kard <misha212032@gmail.com>
2025-03-09 15:09:33 +0500

    Update 11.kt

--- 11.kt
+++ 11.kt
@@ -1,23 +1,25 @@
-import Train.Drive as NewTrain
 fun main() {
     try {
         var OneB = Further(10, 80, "1Б", "Москва", 4, 14, 23)
-        OneB.SetPrice(15.0)
-        println("Введите скорость поезда")
+        OneB.Price()
+        println("Введите среднюю скорость поезда")
         var speed = readln()!!.toDouble()
         OneB.Speed(speed)
         OneB.Info() //вывод информации про прибытие, стоянку и время в пути
         OneB.path() //вывод информации о поезде
+        OneB.Long()
         var L4 = Suburb(10, 10, "Л-4", "Красноуфимск", 0, 16, 9)
-        L4.SetPrice()
-        println("Введите скорость поезда")
+        L4.Time()
+        L4.Price()
+        println("Введите среднюю скорость поезда")
         speed = readln()!!.toDouble()
         L4.Speed(speed)
         L4.Info()
         L4.path()
-        var T10 = NewTrain("T-10", "Ростов", 3, 23, 9)
-        println("Введите скорость поезда")
+        var T10 = Drive("T-10", "Ростов", 3, 23, 9)
+        println("Введите среднюю скорость поезда")
         speed = readln()!!.toDouble()
+        T10.Price()
         T10.Info()
         T10.path()
         T10.Speed(speed)

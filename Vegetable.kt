c60850c5ff5afc3ad40df84bd837412eecb09f84
MK4Kard <misha212032@gmail.com>
2025-03-09 16:26:01 +0500

    Update Vegetable.kt

--- Vegetable.kt
+++ Vegetable.kt
@@ -1,7 +1,6 @@
-import Product.*
 class Vegetable(var fresh: Int, empName: String, empPrice: Double, empEd: String, empType: String, empCount: Int) : Products(empName, empPrice, empEd, empType, empCount) {
     private var calorie: Int = 1
-    public fun SetFresh(fresh: Int){
+    fun SetFresh(fresh: Int){
         if (fresh < 0){
             println("Процент уходит за 0")
         }
@@ -12,7 +11,7 @@ class Vegetable(var fresh: Int, empName: String, empPrice: Double, empEd: String
             this.fresh = fresh
         }
     }
-    public fun SetCal(calorie: Int){
+    fun SetCal(calorie: Int){
         if (calorie <= 0){
             println("Калорий не может быть меньше 1")
         }
@@ -20,14 +19,26 @@ class Vegetable(var fresh: Int, empName: String, empPrice: Double, empEd: String
             this.calorie = calorie
         }
     }
-
     override fun Info() {
-        println("Овощь - ${name}")
-        println("Свежесть ${fresh}%, количество калорий: ${calorie}")
-        println("вид - ${type}, количество: ${count}")
+        if (count > 0){
+            println("Овощь - ${name}")
+            println("Свежесть ${fresh}%, количество калорий: ${calorie}")
+            println("вид - ${type}, количество: ${count}")
+        }
+        else {
+            println("Количество не может быть меньше 0")
+        }
     }
-
     override fun Weight() {
-        println("${price} за 1 ${ed}")
+        if (price > 1) {
+            println("${price} за 1 ${ed}")
+        } else {
+            println("Цена должна быть больше 1")
+        }
+    }
+    override fun Sum(){
+        if (count > 0 && price > 1){
+            println("Итого за ${count} ед надо заплатить ${String.format("%.2f",price*(count/2))} руб")
+        }
     }
 }

add8a4392b79490af6792b548211bc4f233aa84b
MK4Kard <misha212032@gmail.com>
2025-03-09 15:10:11 +0500

    Update Drive.kt

--- Drive.kt
+++ Drive.kt
@@ -1,14 +1,16 @@
-package Train
 open class Drive (var number: String, var point: String, var days: Int, var timeArr: Int, var timePark: Int){
-    var speed: Double = 50.0
-    var price: Double = 345.5
+    var price: Double = 35.5
+    open fun Price(){
+        price *= 10
+    }
+
     open fun path() {
         println("Поезд с номером $number\n" +
                 "Пункт назначения: $point")
     }
 
     open fun Info() {
-        if ((timeArr >= 1 && timePark >= 1) || (timeArr <= 23 && timePark <= 23)){
+        if ((timeArr >= 1 && timePark >= 1)&&(timeArr < 24 && timePark < 24)){
             if (days > 0){
                 println("Дней в пути: $days\n" +
                         "Время прибытия - $timeArr; Время стоянки - $timePark")
@@ -22,21 +24,12 @@ open class Drive (var number: String, var point: String, var days: Int, var time
         }
     }
 
-    open fun SetPrice(price: Double){
-        if (price < 0){
-            println("Цена не может быть отрицательной")
-        }
-        else{
-            this.price = price
-        }
-    }
-
     open fun Speed(speed: Double) {
         if (speed < 35 || speed > 55){
             println("Скорость не входит в диапазон")
         }
         else {
-            println("Скорость поезда: ${speed}")
+            println("Средняя скорость поезда: ${speed}")
         }
     }
 }

8ae59b743025bfc92fb2a7196b7c3d09ddf60e2e
MK4Kard <misha212032@gmail.com>
2025-03-09 16:24:55 +0500

    Update Products.kt

--- Products.kt
+++ Products.kt
@@ -1,29 +1,29 @@
-package Product
 open class Products(var name: String, var price: Double, var ed: String, var type: String, var count: Int) {
-    open fun SetPrice(price: Double){
-        if (price <= 0){
-            println("Товар не может стоить меньше рубля")
-        }
-        else {
-            this.price = price
-        }
-    }
-
-    open fun SetCount(count: Int){
-        if (count < 0){
-            println("Количество не может быть меньше 0")
+    open fun Weight() {
+        if (price > 1){
+            println("${price} за 1 единицу")
         }
         else{
-            this.count = count
+            println("Цена должна быть больше 1")
         }
     }
-
-    open fun Weight() {
-        println("${price} за 1 единицу")
-    }
-
     open fun Info() {
-        println("Товар ${name}")
-        println("вид - ${type}, количество: ${count}")
+        if (count > 0){
+            println("Товар ${name}")
+            println("вид - ${type}, количество: ${count}")
+        }
+        else {
+            println("Количество не может быть меньше 0")
+        }
+    }
+    open fun ExpDate(exp: Int, term: String) {
+        if (exp > 0){
+            println("Срок годности: ${exp} ${term}")
+        }
+    }
+    open fun Sum(){
+        if (count > 0 && price > 1){
+            println("Итого за ${count} ед надо заплатить ${String.format("%.2f",price*count)} руб")
+        }
     }
 }

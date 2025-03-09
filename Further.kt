adf1769451c876919c60d7723dc286bff542b0f5
MK4Kard <misha212032@gmail.com>
2025-03-09 15:10:43 +0500

    Update Further.kt

--- Further.kt
+++ Further.kt
@@ -1,25 +1,12 @@
-import Train.*
 class Further(var typeWag: Int, var tickSale: Int, empNumber: String, empPoint: String, empDays: Int, empTimeA: Int, empTimeP: Int) : Drive(empNumber, empPoint, empDays, empTimeA, empTimeP) {
     var price1: Double = 35.5
-    fun Wag(wag: Int) {
-        if (wag >= 2 || wag < 20){
-            this.typeWag = wag
-        }
-        else {
-            println("Число разновидности вагонов не соответствует норме")
-        }
+    var lenght: Double = 50.0
+    var speed: Double = 1.0
+    fun Long(){
+        lenght = (days*24) * speed
+        println("Длина пути: ${lenght} м")
     }
-
-    fun Sale(sale: Int) {
-        if (sale < 5 || sale > 150) {
-            this.tickSale = sale
-        }
-        else {
-            println("Появление билетов задано не правильно")
-        }
-    }
-
-    override fun SetPrice(price: Double){
+    override fun Price(){
         this.price = days * price1
     }
     override fun Speed(speed: Double) {
@@ -27,7 +14,8 @@ class Further(var typeWag: Int, var tickSale: Int, empNumber: String, empPoint: 
             println("Скорость не входит в диапазон")
         }
         else {
-            println("Скорость поезда: ${speed}")
+            println("Средняя скорость поезда: ${speed}")
+            this.speed = speed
         }
     }
     override fun path() {
